95e62e48465b63b3c63b4de62e0e95ccdd83dc4f
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2024-08-31 06:14:59 +0200

    porting now to the game

--- client.py
+++ client.py
@@ -4,12 +4,12 @@ import socket
 my_pos = (0, 10, 40)
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 9000  # The port used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    #b befor ""
-    l = pickle.dumps(list)
-    s.sendall(l)
-    data = s.recv(1024)
+#b befor ""
+l = pickle.dumps(my_pos)
+s.sendall(l)
+data = s.recv(1024)
 
 print(pickle.loads(data))
